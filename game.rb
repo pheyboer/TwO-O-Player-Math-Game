c1900fc28bb29fe70ce06d8f552095836b4040e9
Peter Heyboer <heyboer.peter@gmail.com>
2025-03-10 21:36:13 -0400

    implementing game logic

--- game.rb
+++ game.rb
@@ -2,3 +2,30 @@
 require_relative 'player'
 require_relative 'question'
 require_relative 'prompt'
+
+# initalize players
+player1 = Player.new("Player 1")
+player2 = Player.new("Player 2")
+cur_player = player1
+
+# Game Loop (while loop)
+while player1.alive? && player2.alive?
+
+  # Generate question
+  question = Question.new
+
+  # ask current player a question
+  answer = Prompt.ask(question.ask_question(cur_player))
+
+  if question.correct_answer?(answer)
+    Prompt.display("You are Correct!")
+  else
+    cur_player.lose_life
+    Prompt.display("That is incorrect :( #{cur_player}! You have #{cur_player.lives} lives remaining")
+  end
+
+  # swap players
+  cur_player = cur_player == player1 ? player2 : player1
+end
+
+
