6a616f189f97965b694dbc88605813b7f7ee7b42
Peter Heyboer <heyboer.peter@gmail.com>
2025-03-10 20:39:17 -0400

    implementing question class

---
+++ question.rb
@@ -0,0 +1,24 @@
+require_relative 'player' # require player
+
+class Question
+  attr_reader :num1, :num2, :ans
+
+  # generate 2 random numbers and get sum
+  def initialize
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+    @ans = @num1 + @num2
+  end
+
+  # return question string for active player
+  def ask_question(player)
+    "#{player.name}, what is #{num1} + #{num2}?"
+  end
+
+  # check if the answer is correct
+  def correct?(input)
+    input.to_i == @ans
+  end
+end
+
+
